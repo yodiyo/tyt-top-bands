6f1b85b6c0602ac02331a5fb7788ec4877457c94
yodiyo <user@Yorick-Browns-MacBook-Pro-07905247756.local>
2018-06-30 00:01:16 +0100

    add role cap

--- tyt-top-bands.php
+++ tyt-top-bands.php
@@ -43,9 +43,30 @@
                 'supports' => array( 'thumbnail'),
                 'taxonomies' => array( '' ),
                 'menu_icon' => 'dashicons-format-audio',
-                'has_archive' => false
+                'has_archive' => false,
+                'capability_type' => array('top_band', 'top_bands'),
+                'map_meta_cap' => true
             )
         );
+    }
+
+
+    // post templates
+    add_filter( 'template_include', 'include_template_function', 1 );
+
+    function include_template_function( $template_path ) {
+        if ( get_post_type() == 'top_bands' ) {
+            if ( is_single() ) {
+                // checks if the file exists in the theme first,
+                // otherwise serve the file from the plugin
+                if ( $theme_file = locate_template( array ( 'page_templates/single-top-bands.php' ) ) ) {
+                    $template_path = $theme_file;
+                } else {
+                    $template_path = plugin_dir_path( __FILE__ ) . '/single-top-bands.php';
+                }
+            } 
+        }
+        return $template_path;
     }
 
     // add artist name as post name
@@ -64,4 +85,73 @@
         wp_update_post($post);
     }
     add_action('acf/save_post', 'update_post', 1); 
+
+    // create top band creator role
+
+    // Register activation hood to add role
+    register_activation_hook( __FILE__, 'tyt_add_band_role' );
+
+    function tyt_add_band_role() {
+        add_role('top_band_creator',
+            'Top Band creator',
+            array(
+                'read' => true,
+                'edit_posts' => false,
+                'delete_posts' => false,
+                'publish_posts' => false,
+                'upload_files' => true,
+            )
+        );
+    }
+
+    // Run the function on admin_init
+    add_action('admin_init', 'remove_profile_menu');
+
+    // Removal function
+    function remove_profile_menu() {
+        global $wp_roles;
+
+        // Remove the menu. Syntax is `remove_submenu_page($menu_slug, $submenu_slug)`
+        remove_submenu_page('users.php', 'profile.php');
+        remove_menu_page('profile.php');
+        
+        /* Remove the capability altogether*/
+        $wp_roles->remove_cap('top_band_creator', 'read');
+    }
+    
+    // Register deactivation hood to remove role
+    register_deactivation_hook( __FILE__, 'tyt_remove_band_role' );
+    
+    function tyt_remove_band_role() {
+        remove_role( 'top_band_creator' );
+    }
+    
+    
+    add_action('admin_init','tyt_add_role_caps',999);
+
+
+    function tyt_add_role_caps() {
+    
+        // Add the roles you'd like to administer the custom post types
+        $roles = array('top_band_creator', 'editor','administrator');
+        
+        // Loop through each role and assign capabilities
+        foreach($roles as $the_role) { 
+    
+            $role = get_role($the_role);
+        
+            $role->add_cap( 'read' );
+            $role->add_cap( 'read_top_band');
+            $role->add_cap( 'read_private_top_band' );
+            $role->add_cap( 'edit_top_band' );
+            $role->add_cap( 'edit_top_bands' );
+            $role->remove_cap( 'edit_others_top_bands' );
+            $role->add_cap( 'edit_published_top_bands' );
+            $role->add_cap( 'publish_top_bands' );
+            //$role->add_cap( 'delete_others_top_bands' );
+            // $role->add_cap( 'delete_private_top_bands' );
+            // $role->add_cap( 'delete_published_top_bands' );
+        }
+    } 
+
 ?>
